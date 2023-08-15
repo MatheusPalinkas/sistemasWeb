d6662c87fdba973e51a7f4934efcc1731433d5a7
MatheusPalinkas <matheuspalinkas31@gmail.com>
2023-08-15 00:57:27 -0300

    TP01

---
+++ sistemasWeb2/TP01/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel;
+using TP01;
+using TP01.Controller;
+
+var controller = new BookController();
+
+controller.DesmostrarMetodos();
+
+Console.WriteLine("\n\n\n\n");
+
+IWebHost host = new WebHostBuilder()
+    .UseKestrel()
+    .UseStartup<Startup>()
+    .Build();
+
+host.Run();
